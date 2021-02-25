7b1fe2458ed743ae33beaa2d35e0ec03d955d1b7
VerdetVlad <v.verdet@yahoo.ro>
2021-02-25 17:28:25 +0200

    added movePiece(x1,y1,x2,y2)
    You can move the pieces but there is very little error checking

--- logic/logic.c
+++ logic/logic.c
@@ -11,80 +11,129 @@
 #define bQ -5
 #define bK -6
 
-#define wP  1
-#define wN  2 
-#define wB  3
-#define wR  4
-#define wQ  5
-#define wK  6
+#define wP 1
+#define wN 2
+#define wB 3
+#define wR 4
+#define wQ 5
+#define wK 6
 
+void resizeCoord(int *x1, int *y1, int *x2, int *y2);
+int basicCheck(int x1, int y1, int x2, int y2);
 
 int board[8][8];
-
 
 void setStartingBoard()
 {
     //Pawns
-    for(int i=0;i<8;i++) 
+    for (int i = 0; i < 8; i++)
     {
-        board[1][i]=bP;
-        board[6][i]=wP;
+        board[i][1] = wP;
+        board[i][6] = bP;
     }
 
     //Knights
-    board[0][1]=board[0][6]=bN;
-    board[7][1]=board[7][6]=wN;
+    board[1][0] = board[6][0] = wN;
+    board[1][7] = board[6][7] = bN;
 
     //Bishops
-    board[0][2]=board[0][5]=bB;
-    board[7][2]=board[7][5]=wB;
+    board[2][0] = board[5][0] = wB;
+    board[2][7] = board[5][7] = bB;
 
     //Rooks
-    board[0][0]=board[0][7]=bR;
-    board[7][0]=board[7][7]=wR;
+    board[0][0] = board[7][0] = wR;
+    board[0][7] = board[7][7] = bR;
 
     //Queens
-    board[0][4]=bQ;
-    board[7][4]=wQ;
+    board[4][0] = wQ;
+    board[4][7] = bQ;
 
     //Kings
-    board[0][3]=bK;
-    board[7][3]=wK;
-
-
+    board[3][0] = wK;
+    board[3][7] = bK;
 }
 
 //copies the board into the board given as a parameter
 void getBoard(int auxBoard[8][8])
 {
-
-    memcpy(auxBoard,board, 8*8*sizeof(int));
-
+    memcpy(auxBoard, board, 8 * 8 * sizeof(int));
 }
 
 
 
 
+//returns 0 on invalid move, otherwise returns 1
+// x1,y1 - initial position, can have values between [1,8]
+// x2,y2 - new position
+int movePiece(int x1, int y1, int x2, int y2)
+{
+    resizeCoord(&x1, &y1, &x2, &y2);
+    if (!basicCheck(x1, y1, x2, y2))
+        return 0;
 
+    int pieceType = board[x1][y1];
 
+    board[x2][y2] = pieceType;
+    board[x1][y1] = 0;
 
+    return 1;
+}
 
+//makes the coord from user frendly to array frendly
+void resizeCoord(int *x1, int *y1, int *x2, int *y2)
+{
+    *x1 -= 1;
+    *y1 -= 1;
+    *x2 -= 1;
+    *y2 -= 1;
+}
 
+//checks general things that are valid for all pieces
+// return 0 on incorrect imput, 1 otherwise
+int basicCheck(int x1, int y1, int x2, int y2)
+{
+    //not outside of board check
+    if (x1 > 7 || y1 > 7 || x2 > 7 || y2 > 7)
+        return 0;
+    if (x1 < 0 || x1 < 0 || x1 < 0 || x1 < 0)
+        return 0;
 
+    //non empty start check
+    if (board[x1][y1] == 0)
+        return 0;
+
+    //not ending on same color piece check
+    if (board[x2][y2] != 0)
+    {
+        int pieceColorStart = (board[x1][y1] > 0) ? 1 : -1; //1 white, -1 black
+        int pieceColorEnd = (board[x2][y2] > 0) ? 1 : -1;
+        
+
+        if (pieceColorStart == pieceColorEnd)
+            return 0;
+    }
+
+    return 1;
+}
 
 //for testing
 void printBoard(int board[8][8])
 {
-    for(int i =0;i<8;i++)
+    for (int i = 7; i >= 0; i--)
     {
-        for(int j=0;j<8;j++) {
-            
-            if(board[i][j]< 0) printf(" %d",board[i][j]);
-            else printf("  %d",board[i][j]);
+        for (int j = 0; j < 8; j++)
+        {
+
+            if (board[j][i] < 0)
+                printf(" %d", board[j][i]);
+            else
+                printf("  %d", board[j][i]);
         }
-        printf("\n");
-    }
+
+        printf(" |%d\n",i+1);
+    } 
+    for (int j = 0; j < 8; j++) printf("  _");
+    printf("\n");
+    for (int j = 0; j < 8; j++) printf("  %d",j+1);
+    printf("\n");
 }
-
-
-
--- logic/logic.h
+++ logic/logic.h
@@ -1,6 +1,7 @@
 #if !defined(CHESS_LOGIC_H)
 #define CHESS_LOGIC_H
 
+
 #define empty 0
 
 #define bP -1
@@ -23,8 +24,14 @@ void setStartingBoard();
 //copies the board into the board given as a parameter
 void getBoard(int auxBoard[8][8]);
 
+//returns 0 on invalid move, otherwise returns 1
+// x1,y1 - initial position, can have values between [1,8]
+// x2,y2 - new position
+int movePiece(int x1, int y1, int x2, int y2);
+
 //a basic print of the board
 //for testing, should be removed in the future
 void printBoard(int board[8][8]);
+
 
 #endif // CHESS_LOGIC_H
